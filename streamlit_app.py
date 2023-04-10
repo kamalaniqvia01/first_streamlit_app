d9ad2eb76e271463336f4bf214d1e864a5601b27
kamalaniqvia01 <130389987+kamalaniqvia01@users.noreply.github.com>
2023-04-10 13:25:40 -0400

    Update streamlit_app.py
    S3 Upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import streamlit
 streamlit.title('Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('Kale Smoothie')
 streamlit.text('Banana Smoothie')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
