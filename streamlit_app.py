18388bfd43ff37b8bc6bb6349608fa2fd878fefd
kamalaniqvia01 <130389987+kamalaniqvia01@users.noreply.github.com>
2023-04-10 13:22:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,6 @@ import streamlit
 streamlit.title('Healthy Diner')
 
 streamlit.header('Breakfast Menu')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('Kale Smoothie')
 streamlit.text('Banana Smoothie')
