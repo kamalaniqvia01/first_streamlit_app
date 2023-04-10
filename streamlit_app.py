26bba24d564fcca5970af5e4b91963547442ddf8
kamalaniqvia01 <130389987+kamalaniqvia01@users.noreply.github.com>
2023-04-10 13:21:30 -0400

    Update streamlit_app.py
    Fixed Spelling Mistake

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Kale Smoothoie')
-streamlit.text('Banana Smoothoie')
+streamlit.text('Kale Smoothie')
+streamlit.text('Banana Smoothie')
