c1c9b6387c853a7f006ef07f3cf2148918ac2154
kamalaniqvia01 <130389987+kamalaniqvia01@users.noreply.github.com>
2023-04-13 15:56:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,14 +40,14 @@ def insert_row_snowflake(new_fruit):
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-  else:
-    back_from_function=get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function) 
-  
+
+#   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#   if not fruit_choice:
+#     streamlit.error("Please select a fruit to get information.")
+#   else:
+#     back_from_function=get_fruityvice_data(fruit_choice)
+#     streamlit.dataframe(back_from_function) 
+try:  
   add_my_fruit = streamlit.text_input('What fruit would you like to add?')
   if not add_my_fruit:
     streamlit.error("Please type a fruit you would like to add.")
